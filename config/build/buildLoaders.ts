eccc87b7e256720a1de6e53992ad8c5945102417
Pavel Gladyshev <gladyshevpv@gmail.com>
2022-09-15 17:13:31 +0300

    Декомпозиция конфига. Опции конфигурации

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,17 @@
+import {RuleSetRule} from "webpack";
+
+
+
+export function buildLoaders(): RuleSetRule[] {
+    
+    const typescriptLoader = {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+
+    }
+
+    return [
+        typescriptLoader,
+      ]
+}
