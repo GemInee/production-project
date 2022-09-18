36ab9ff732d52da416f0c2884470857a3aef6a7e
Pavel Gladyshev <gladyshevpv@gmail.com>
2022-09-18 07:35:23 +0300

    Настраиваем css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,19 +1,31 @@
+import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { RuleSetRule } from "webpack";
 
-export function buildLoaders(): RuleSetRule[] {
-  
+export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
       // Creates `style` nodes from JS strings
-      "style-loader",
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
       // Translates CSS into CommonJS
-      "css-loader",
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) =>
+              Boolean(resPath.includes(".module.scss")),
+            localIdentName: isDev 
+            ? "[path][name]__[local]--[hash:base64:5]" 
+            : "[hash:base64:8]",
+          },
+        },
+      },
       // Compiles Sass to CSS
       "sass-loader",
     ],
   };
-  
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
