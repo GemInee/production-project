c2eb9e0b1793b107ba62adb617b455dd75d151fd
Pavel Gladyshev <gladyshevpv@gmail.com>
2022-09-18 11:25:35 +0300

    Организация стилей. Добавляем темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
 import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
-      <Link to={'/about'}>О сайте</Link>
-      <Link to={'/'}>Главная</Link>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={"/about"}>О сайте</Link>
+      <Link to={"/"}>Главная</Link>
       <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path={"/about"} element={<AboutPageAsync/>}/>
-        <Route path={"/"} element={<MainPageAsync/>}/>
-      </Routes>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
       </Suspense>
-
     </div>
   );
 };
