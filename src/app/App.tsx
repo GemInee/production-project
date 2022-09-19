a9e7db89f6b76052a44fdc93c015a6c13fce2648
Pavel Gladyshev <gladyshevpv@gmail.com>
2022-09-19 17:28:26 +0300

    Navbar. Шаблоны для разработки. Первый UI Kit компонент

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import { useTheme } from "./providers/ThemeProvider";
 import "./styles/index.scss";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar/>
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/about"}>О сайте</Link>
-      <Link to={"/"}>Главная</Link>
       <AppRouter />
     </div>
   );
