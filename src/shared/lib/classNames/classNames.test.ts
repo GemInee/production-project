5658fec9b6a43bb20a9f92ccb617004974acd73f
Pavel Gladyshev <gladyshevpv@gmail.com>
2022-09-25 14:15:06 +0300

    Тестовая среда. Настраиваем Jest. Пишем первый тест

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,43 @@
+import { classNames } from "./classNames"
+
+describe('classNames', () => {
+    test('whith only first param', () => {
+        expect(classNames('someClass')).toBe('someClass ');
+    });
+
+    test('with additional classes', () => {
+        const expected = 'someClass class1 class2 ';
+        expect(classNames(
+            'someClass',
+            {},
+            ['class1', 'class2']
+        )).toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'someClass class1 class2 hover,selected';
+        expect(classNames(
+            'someClass',
+            { hover: true, selected: true },
+            ['class1', 'class2']
+        )).toBe(expected);
+    });
+
+    test('with mods false', () => {
+        const expected = 'someClass class1 class2 hover';
+        expect(classNames(
+            'someClass',
+            { hover: true, selected: false },
+            ['class1', 'class2']
+        )).toBe(expected);
+    });
+
+    test('with mods undef', () => {
+        const expected = 'someClass class1 class2 hover';
+        expect(classNames(
+            'someClass',
+            { hover: true, selected: undefined },
+            ['class1', 'class2']
+        )).toBe(expected);
+    });
+})
