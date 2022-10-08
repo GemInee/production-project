2e59f2ab38955087187631b0b4b7508ffeb2f935
GemInee <gladyshevpv@gmail.com>
2022-10-09 01:51:19 +0700

    eslint code format

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,5 +1,5 @@
 import webpack from 'webpack';
-import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildCssLoaders } from './loaders/buildCssLoaders';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
@@ -28,7 +28,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-    const cssLoader = buildCssLoader(isDev);
+    const cssLoader = buildCssLoaders(isDev);
 
     // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
